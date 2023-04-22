29a01604eca5b726a9b47b00415269471afb6b87
jsungl <wotjd931026@gmail.com>
2023-04-23 04:17:22 +0900

    로그 필터 생성

---
+++ src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,25 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+/**
+ * 필터 설정
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter()); //LogFilter를 등록
+        filterRegistrationBean.setOrder(1); //필터 순서
+        filterRegistrationBean.addUrlPatterns("/*"); //필터를 적용할 URL 패턴. 모든 URL에 적용
+        return filterRegistrationBean;
+    }
+
+}
